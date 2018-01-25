2f9d9e5dd848a3bad4bdd0210c73bb90c13a3791
sqweek <sqweek@gmail.com>
2018-01-25 23:54:33 +0800

    win32: prevent selection of non-fs directories
    Avoids an empty directory name being returned if a user browses to one
    of the special windows shell virtual folders like "This PC".
    
    Fixes #12.

--- dlgs_windows.go
+++ dlgs_windows.go
@@ -124,7 +124,7 @@ type dirdlg struct {
 }
 
 func selectdir(b *DirectoryBuilder) (d dirdlg) {
-	d.bi = &w32.BROWSEINFO{}
+	d.bi = &w32.BROWSEINFO{Flags: w32.BIF_RETURNONLYFSDIRS}
 	if b.Dlg.Title != "" {
 		d.bi.Title, _ = syscall.UTF16PtrFromString(b.Dlg.Title)
 	}
